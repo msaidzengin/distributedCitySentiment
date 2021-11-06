6adb149ca06dafdaec1350ca490fdf45133726d4
msaidzengin <m.zengin@etu.edu.tr>
2021-11-06 09:58:37 +0300

    baseline code

---
+++ baseline/code.py
@@ -0,0 +1,26 @@
+import glob
+import ktrain
+import datetime
+
+start_time = datetime.datetime.now()
+
+all_files = glob.glob("../data/*.txt") 
+print("Total number of files:", len(all_files))
+
+#model = ktrain.load_predictor("../model")
+print("Sentiment model loaded")
+
+cities = {}
+for filename in all_files:
+    print("Opening file:", filename)
+    # cities[filename] = []
+    with open(filename) as myfile:
+        tweets = [next(myfile) for x in range(10)]
+    
+    # model.predict(tweets)
+    # cities[filename] = preedictions
+
+# json.dump(cities, open("result.json", "w"))
+
+end_time = datetime.datetime.now()
+print("Total time:", end_time - start_time)
