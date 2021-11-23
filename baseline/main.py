7b417f3172a84517432716ecd96c2ef23b68105c
msaidzengin <m.zengin@etu.edu.tr>
2021-11-24 00:48:50 +0300

    results

--- baseline/main.py
+++ baseline/main.py
@@ -1,6 +1,7 @@
 import glob
 import datetime
 import ktrain
+import json
 
 start_time = datetime.datetime.now()
 
@@ -12,15 +13,18 @@ print("Sentiment model loaded")
 
 cities = {}
 for filename in all_files:
-    print("Opening file:", filename)
-    # cities[filename] = []
-    with open(filename) as myfile:
-        tweets = [next(myfile) for x in range(10)]
-    
-    # model.predict(tweets)
-    # cities[filename] = preedictions
 
-# json.dump(cities, open("result.json", "w"))
+    city_name = filename.split("/")[-1].split(".")[0]
+    print("Opening file:", filename, city_name)
+
+    with open(filename) as myfile:
+        tweets = [next(myfile) for x in range(1)]
+    
+    predictions = model.predict(tweets)
+    cities[city_name] = predictions
+
+with open('result.json', 'w', encoding='utf-8') as f:
+    json.dump(cities, f, ensure_ascii=False, indent=4)
 
 end_time = datetime.datetime.now()
 print("Total time:", end_time - start_time)
