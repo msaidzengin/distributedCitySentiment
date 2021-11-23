efd35fb283b87d7f2edcfa2cf1df635ed70dd5dc
msaidzengin <m.zengin@etu.edu.tr>
2021-11-24 00:54:58 +0300

    percents

--- baseline/main.py
+++ baseline/main.py
@@ -8,23 +8,41 @@ start_time = datetime.datetime.now()
 all_files = glob.glob("../data/*.txt") 
 print("Total number of files:", len(all_files))
 
-model = ktrain.load_predictor("../model")
-print("Sentiment model loaded")
-
-cities = {}
+all_tweets = {}
 for filename in all_files:
 
     city_name = filename.split("/")[-1].split(".")[0]
     print("Opening file:", filename, city_name)
 
     with open(filename) as myfile:
-        tweets = [next(myfile) for x in range(1)]
+        tweets = [next(myfile) for x in range(2)]
     
-    predictions = model.predict(tweets)
-    cities[city_name] = predictions
+    all_tweets[city_name] = tweets
+
+
+model = ktrain.load_predictor("../model")
+print("Sentiment model loaded")
+
+results = {}
+for city, tweet in all_tweets.items():
+    print("\nCity:", city)
+    res = []
+    for t in tweet:
+        print(t)
+        prediction = model.predict(t)
+        res.append(prediction)
+    results[city] = res
+
 
 with open('result.json', 'w', encoding='utf-8') as f:
-    json.dump(cities, f, ensure_ascii=False, indent=4)
+    json.dump(results, f, ensure_ascii=False, indent=4)
+
+percents = {}
+for city, res in results.items():
+    percents[city] = sum(res) / len(res)
+
+with open('percents.json', 'w', encoding='utf-8') as f:
+    json.dump(percents, f, ensure_ascii=False, indent=4)
 
 end_time = datetime.datetime.now()
 print("Total time:", end_time - start_time)
